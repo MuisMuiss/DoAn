aec43677e830eb35213dc4984c0c957639626478
MuisMuiss <binhnopiwtf@gmail.com>
2024-10-11 22:50:02 +0700

    sửa admin

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -15,20 +15,20 @@ class AdminController extends Controller
     public function viewhome(){
         return view('admin.home');
     }
-    public function login(Request $request)
-    {
-        $username = $request->input('username');
-        $password = $request->input('password');
-        $tk = DB::table('nguoi_dung')->where('vai_tro', '=', 1)->select("*")->get();
-        foreach ($tk as $row) {
-            if ($row->ten_dang_nhap == $username && $row->mat_khau == md5($password)) {
-                $dt = DB::table('san_pham')->select('*');
-                $dt = $dt->get();
-                setcookie("id", $row->Id, time() + 3600);
-                return view("home", compact('dt'));
-                break;
-            }
-        }
-        return view("login", ['loginError' => "Sai mật khẩu hoặc tài khoản", 'SignUpError' => ""]);
-    }
+    // public function login(Request $request)
+    // {
+    //     $username = $request->input('username');
+    //     $password = $request->input('password');
+    //     $tk = DB::table('nguoi_dung')->where('vai_tro', '=', 1)->select("*")->get();
+    //     foreach ($tk as $row) {
+    //         if ($row->ten_dang_nhap == $username && $row->mat_khau == md5($password)) {
+    //             $dt = DB::table('san_pham')->select('*');
+    //             $dt = $dt->get();
+    //             setcookie("id", $row->Id, time() + 3600);
+    //             return view("home", compact('dt'));
+    //             break;
+    //         }
+    //     }
+    //     return view("login", ['loginError' => "Sai mật khẩu hoặc tài khoản", 'SignUpError' => ""]);
+    // }
 }
